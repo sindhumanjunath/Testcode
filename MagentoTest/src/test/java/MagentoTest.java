6dad63de08d1e46caa5713d7764ba1a350fded9f
Admin <Admin@Admin-PC>
2018-10-13 21:38:29 +0530

    1st push

---
+++ MagentoTest/src/test/java/MagentoTest.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class MagentoTest {
+
+	@Test
+	public void posCredential1() throws Exception
+	{
+		String url="https://magento.com";
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(url);
+		Main m = new Main(driver);
+		m.clickOnMyacc();
+
+		Thread.sleep(5000);
+
+		Login login = new Login(driver);
+		login.typeEmail("suchendra.abc@gmail.com");
+		login.typePwd("Welcome123");
+		login.clickOnLogin();
+
+		Thread.sleep(2000);
+
+		Logout logout = new Logout(driver);
+		logout.clickOnLogout();
+
+		Thread.sleep(2000);
+
+		driver.quit();
+	}
+}

d27de75cb359839a422f3a545dab77986ab06a6f
Admin <Admin@Admin-PC>
2018-10-15 13:10:32 +0530

    PageObjectModel

---
+++ PageObjectModel/src/com/abc/magentoTest/MagentoApp.java
@@ -0,0 +1,36 @@
+package com.abc.magentoTest;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import com.abc.magentoObject.*;
+
+public class MagentoApp {
+
+	public static void main(String[] args) throws Exception 
+	{
+		String url="https://magento.com";
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(url);
+		Main m = new Main(driver);
+		m.clickOnMyacc();
+
+		Thread.sleep(5000);
+
+		Login login = new Login(driver);
+		login.typeEmail("subramanyaraj87@gmail.com");
+		login.typePwd("Welcome123");
+		login.clickOnLogin();
+
+		Thread.sleep(2000);
+
+		Logout logout = new Logout(driver);
+		logout.clickOnLogout();
+
+		Thread.sleep(2000);
+
+		driver.quit();
+	}
+}
